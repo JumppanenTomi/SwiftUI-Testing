d3672b066af6eca8fcf73ec8492fcaf67e4758d1
Tomi Jumppanen <tomi.jumppanen@hotmail.com>
2023-04-02 00:51:00 +0300

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,45 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by iosdev on 1.4.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            MapView()
+                .ignoresSafeArea(edges: .top)
+                .frame(height: 300)
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -300)
+            Spacer()
+            VStack (alignment: .leading){
+                Text("Turtle Rock")
+                    .font(.title)
+                HStack {
+                    Text("Joshua Tree National Park")
+                    Spacer()
+                    Text("California")
+                }
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+                Divider()
+                Text("About Turtle Rock")
+                    .font(.title2)
+                Text("Description is here")
+            }
+            .padding()
+            Spacer()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
