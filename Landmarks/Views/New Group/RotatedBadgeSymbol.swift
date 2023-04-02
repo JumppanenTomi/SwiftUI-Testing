505f27c89d52784b1007940433a1626b4b74b257
Tomi Jumppanen <tomi.jumppanen@hotmail.com>
2023-04-02 15:41:39 +0300

    There is problem in my Xcode that refuses to allow renaming this folder into badges. But everything seems to work now.

---
+++ Landmarks/Views/New Group/RotatedBadgeSymbol.swift
@@ -0,0 +1,23 @@
+//
+//  RotatedBadgeSymbol.swift
+//  Landmarks
+//
+//  Created by Tomi Jumppanen on 2.4.2023.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: Angle(degrees: 5))
+    }
+}
